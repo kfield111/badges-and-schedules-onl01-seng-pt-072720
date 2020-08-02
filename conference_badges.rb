7c77371684245e6134f0f4cccfbd3a67398d9fc4
Kelin Field <kfield111@hotmail.com>
2020-08-02 00:55:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms (attendees)
   rooms = [1, 2, 3, 4, 5, 6, 7]
   attendees.each do |name|
     current_room = rooms[0]
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{name}!"
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{current_room}!"
     current_room += 1
   end
   return room_assignments
