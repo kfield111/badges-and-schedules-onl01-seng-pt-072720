f8718f948f9fb30d031c1c964bf26c2ead55099a
Kelin Field <kfield111@hotmail.com>
2020-08-02 00:19:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees.each do {|name| return "Hello, my name is #{name}." = badge}
+  attendees.each do {|name| return "Hello, my name is #{name}."}
   return badges = [badges]
 
 end
