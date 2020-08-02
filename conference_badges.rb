75c509fd6ec3bfd2c732e66d12d2e1924b950bfa
Kelin Field <kfield111@hotmail.com>
2020-08-02 01:22:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,5 @@ def printer (attendees)
 badges_and_room_assignments = []
 batch_badge_creator.each do |badge|
   badges_and_room_assignments << puts "#{badges}"
-assign_rooms.each do |rooms|
-  badges_and_room_assignments << puts "#{room_assignments}"
  end
 end
